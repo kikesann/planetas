3025ee94dd1e92f88576438a3c94c5710d44ffa5
Enrique <e.resendiz.r688@edu.utc.mx>
2026-02-12 11:30:15 -0600

    primer despliegue

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require ('express');
+const { read } = require('fs');
+const app =express ();  
+const path = require('path'); 
+
+
+app.use(express.static(path.join(__dirname,'views/planetas')));
+app.set('view engine','ejs');
+
+app.get('/sistema-solar',(req,res) => {
+     res.render('planetas/planetas');   
+     
+       console.log('pagina abierta ');
+});
+
+
+
+
+app.get('/mercurio',(req,res) => {
+     res.render('planetas/mercurio');   
+     
+       console.log('Pagina mercurio cargada');
+});
+app.get('/venus',(req,res) => {
+     res.render('planetas/venus');   
+     
+       console.log('Pagina venus cargada');
+});
+app.get('/tierra',(req,res) => {
+     res.render('planetas/tierra');   
+     
+       console.log('Pagina tierra cargada');
+});
+app.get('/marte',(req,res) => {
+     res.render('planetas/marte');   
+     
+       console.log('Pagina marte cargada');
+});
+app.get('/jupiter',(req,res) => {
+     res.render('planetas/jupiter');   
+     
+       console.log('Pagina jupiter cargada');
+});
+app.get('/saturno',(req,res) => {
+     res.render('planetas/saturno');   
+     
+       console.log('Pagina saturno cargada');
+});
+app.get('/urano',(req,res) => {
+     res.render('planetas/urano');   
+     
+       console.log('Pagina urano cargada');
+});
+app.get('/neptuno',(req,res) => {
+     res.render('planetas/neptuno');   
+     
+       console.log('Pagina neptuno cargada');
+});
+
+
+
+
+
+
+
+
+
+const server = app.listen(4000, function() {
+    console.log('listening to port 4000')
+    console.log('Hola mundo ')
+});
+
+
